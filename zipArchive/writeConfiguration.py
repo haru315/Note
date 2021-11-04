14170d3f48981067418f2a0c837756f8279d0f28
haru315 <69494667+haru315@users.noreply.github.com>
2021-11-04 14:19:47 +0900

    Add files via upload

---
+++ zipArchive/writeConfiguration.py
@@ -0,0 +1,55 @@
+import json
+# コンフィグ関数
+
+# y/n 関数
+def yes_no_input(explanation):
+    while True:
+        choice = input(f"{explanation} [y/N]: ").lower()
+        if choice in ['y', 'ye', 'yes']:
+            return True
+        elif choice in ['n', 'no']:
+            return False
+
+information = {}
+
+redo = False
+while(not redo):
+    information.clear()
+
+    # 新しいapp_name を入れる
+    # ・ モデルの name と path を入力 複数可
+    information["app_name"] = input('app_name:')
+    models = []
+    loop = True
+    while(loop):
+        models.append({
+            "mode_name":input('mode_name:'),
+            "mode_path":input('mode_path:')
+            })
+        loop = yes_no_input('Is there still?')
+    information["models"] = models
+
+    # env の name host と name port を入力
+    # ・ key と value を入力 複数可
+    information["name_host"] = input('name_host:')
+    information["name_port"] = input('name_port:')
+    envs = []
+    loop = True
+    while(loop):
+        envs.append({
+            "key":input('key:'),
+            "value":input('value:')
+            })
+        loop = yes_no_input('Is there still?')
+    information["envs"] = envs
+
+
+    print(information)
+    redo = yes_no_input('Is this okay?')
+
+# JSONオブジェクトを作成
+j = json.dumps(information)
+
+# 保存
+with open('test_new.json', mode='wt', encoding='utf-8') as f:
+  json.dump(information, f, ensure_ascii=False, indent=2)
